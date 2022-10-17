cf029196e0c915914b0728e78bd4279451a32374
mixa <mihail.novokreshenov@pitch-it.ru>
2022-10-17 10:42:18 +0300

    попытка написать конфиг

--- Service/AuthService.php
+++ Service/AuthService.php
@@ -4,4 +4,19 @@ namespace Mixasmix\AuthBundle\Service;
 
 class AuthService
 {
+    public function __construct(
+        string $authClientId,
+        string $authClientSecret,
+        string $authUrlAuthoruze,
+        string $authUrlAccessToken,
+        string $authUrlResourcesOwnerDetails,
+    ) {
+        dd(
+        $authClientId,
+        $authClientSecret,
+        $authUrlAuthoruze,
+        $authUrlAccessToken,
+        $authUrlResourcesOwnerDetails,
+        );
+    }
 }
