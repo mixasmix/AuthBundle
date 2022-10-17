2fd87d37680153b3df6a94fe1d4136afe295258d
mixa <mihail.novokreshenov@pitch-it.ru>
2022-10-17 12:51:04 +0300

    Добавил типы

--- Service/AuthService.php
+++ Service/AuthService.php
@@ -2,13 +2,20 @@
 
 namespace Mixasmix\AuthBundle\Service;
 
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\GenericProvider;
+use League\OAuth2\Client\Token\AccessToken;
 use Mixasmix\AuthBundle\DTO\LinkData;
+use Mixasmix\AuthBundle\DTO\UserData;
 use Mixasmix\AuthBundle\Enum\AuthorizeResponseType;
+use Mixasmix\AuthBundle\Enum\GrantType;
+use Mixasmix\AuthBundle\Exception\RequiredParameterEmptyException;
 
 class AuthService
 {
     private GenericProvider $provider;
+
+    private ?AccessToken $tokenData = null;
 
     public function __construct(
         string $authServiceUrl,
@@ -41,5 +48,46 @@ class AuthService
             ]),
             state: $this->provider->getState(),
         );
+    }
+
+    /**
+     * @throws RequiredParameterEmptyException
+     * @throws IdentityProviderException
+     */
+    public function getToken(
+        GrantType $grant,
+        ?string $username = null,
+        ?string $password = null,
+        ?string $code = null,
+        ?string $refreshToken = null,
+    ): AccessToken {
+        if (is_null($this->tokenData)) {
+            $this->tokenData = $this->provider->getAccessToken(
+                grant: $grant,
+                options: match (true) {
+                    $grant->isClientCredentials() => [],
+                    $grant->isPassword() => [
+                        'username' => $username ?? throw new RequiredParameterEmptyException('Не передано имя пользователя'),
+                        'password' => $password ?? throw new RequiredParameterEmptyException('Не передан пароль пользователя'),
+                    ],
+                    $grant->isAuthorizationCode() => [
+                        'code' => $code ?? throw new RequiredParameterEmptyException('Не передан авторизационный код'),
+                    ],
+                    $grant->isRefreshToken() => [
+                        'refresh_token' => $refreshToken ?? throw new RequiredParameterEmptyException('Не передан Refresh Token'),
+                    ],
+                }
+            );
+        }
+
+        return $this->tokenData;
+    }
+
+    /**
+     * @return array<UserData>
+     */
+    public function getUserData(): array
+    {
+
     }
 }
