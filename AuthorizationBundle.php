aa5f5e1d11230f166cb4dbc287c101b4b2fa1ab2
mixa <mihail.novokreshenov@pitch-it.ru>
2022-10-17 11:30:53 +0300

    попытка написать конфиг

--- AuthorizationBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mixasmix\AuthBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AuthorizationBundle extends Bundle
-{
-}
--- Service/AuthService.php
+++ Service/AuthService.php
@@ -5,21 +5,12 @@ namespace Mixasmix\AuthBundle\Service;
 class AuthService
 {
     public function __construct(
-        $authProdavayApiUrl
-        // string $authClientId,
-        // string $authClientId,
-        // string $authClientSecret,
-        // string $authUrlAuthoruze,
-        // string $authUrlAccessToken,
-        // string $authUrlResourcesOwnerDetails,
+        string $authClientId,
+        string $authClientSecret,
+        string $authUrlAuthoruze,
+        string $authUrlAccessToken,
+        string $authUrlResourcesOwnerDetails,
     ) {
-        dd($authProdavayApiUrl);
-        // dd(
-        // $authClientId,
-        // $authClientSecret,
-        // $authUrlAuthoruze,
-        // $authUrlAccessToken,
-        // $authUrlResourcesOwnerDetails,
-        // );
+
     }
 }
