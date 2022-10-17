4bdf3cf79338c4fb2d14157eeebd099977ae39f4
mixa <mihail.novokreshenov@pitch-it.ru>
2022-10-17 10:19:29 +0300

    попытка написать конфиг

---
+++ DependencyInjection/AuthBundleExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mixasmix\AuthBundle\DependencyInjection;
+
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class AuthBundleExtension extends Extension
+{
+    private const BUNDLE_NAME = 'authorization';
+
+    /**
+     * @throws Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $container->setParameter(self::BUNDLE_NAME . '.client_id', $config['client_d']);
+    }
+}
