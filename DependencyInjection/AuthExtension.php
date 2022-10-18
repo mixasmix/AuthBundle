6459d53e314a77eb31e47a692232d4725c40b796
mixa <mihail.novokreshenov@pitch-it.ru>
2022-10-18 12:33:40 +0300

    Перенес конфиг в xml

--- DependencyInjection/AuthExtension.php
+++ DependencyInjection/AuthExtension.php
@@ -2,23 +2,24 @@
 
 namespace Mixasmix\AuthBundle\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class AuthExtension extends Extension
 {
     private const ALIAS = 'auth';
 
     /**
-     * @inheritDoc
+     * @throws Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $loader->load('services.yaml');
+        $loader->load('services.xml');
 
         $config = $this->processConfiguration(new Configuration(), $configs);
 
